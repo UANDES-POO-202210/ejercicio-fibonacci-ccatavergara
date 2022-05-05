cb68e65d9bfb24c272d310cd109abad3435ae497
Cata Vergara <mcvergara1@miuandes.cl>
2022-05-04 21:09:37 -0400

    Secuencia fibonacci final

---
+++ main.js
@@ -0,0 +1,18 @@
+function fibonacci(i){
+    if(i==1 | i==0){
+        return i;
+    }
+    else{
+        return fibonacci(i-1) + fibonacci(i-2);
+    }
+};
+
+var fs = require('fs');
+fs.readFile("number.txt", 'utf8', function(err, data) {
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log("El numero fibonacci de %d es %d",data,fibonacci(data));
+});
+
